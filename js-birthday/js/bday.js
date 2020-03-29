5d244683862eaca442cac3fd9383ccfa878e3f04
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-03-28 18:56:18 -0600

    Update bday.js

--- js-birthday/js/bday.js
+++ js-birthday/js/bday.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 var name = prompt("Enter your name:");
 var age = prompt("Enter your age:");
 var month = prompt("Enter your birth month:");
-=======
-var name = prompt("Enter your name:");
-var age = prompt("Enter your age:");
-var month = prompt("Enter your birth month:");
->>>>>>> 2582851a0fb94a7840fb430f0767ce97fcc07c7b
+
 alert("Hello " + name + ". You are " + age + " years old, and you were born in " + month);
