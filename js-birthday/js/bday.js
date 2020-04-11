b000927211b96ad58e69a23c9e6b56b9f4d74aea
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-11 13:25:45 -0600

    Update bday.js

--- js-birthday/js/bday.js
+++ js-birthday/js/bday.js
@@ -1,5 +1,7 @@
-var name = prompt("Enter your name:");
-var age = prompt("Enter your age:");
-var month = prompt("Enter your birth month:");
-
-alert("Hello " + name + ". You are " + age + " years old, and you were born in " + month);
+var noun1 = prompt("Enter a noun: ");
+var noun2 = prompt("Enter a noun: ");
+var verb1 = prompt("Enter a verb: ");
+var verb2 = prompt("Enter a verb: ");
+var adj1 = prompt("Enter an adjective: ");
+var adj1 = prompt("Enter an adjective: ");
+alert("There was a " + adj1 + " boy named Bryan. Bryan had to " + verb1 + ". Before he could do that, he needed to find his " + noun2 + ");
