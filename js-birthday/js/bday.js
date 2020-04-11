6466e276333757fdc84b5695791cc181342d8fdb
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-11 13:36:53 -0600

    Update bday.js

--- js-birthday/js/bday.js
+++ js-birthday/js/bday.js
@@ -4,4 +4,4 @@ var verb1 = prompt("Enter a verb: ");
 var verb2 = prompt("Enter a verb: ");
 var adj1 = prompt("Enter an adjective: ");
 var adj1 = prompt("Enter an adjective: ");
-alert("There was a " + adj1 + " boy named Bryan. Bryan had to " + verb1 + ". Before he could do that, he needed to find his " + noun2 + ");
+alert("There was a " + adj1 + " boy named Bryan. Bryan had to " + verb1 + ". Before he could do that, he needed to find his " + adj2 + " " + noun2 + "Afterwards, Bryan went for a " + verb2 + " with his " + noun1);
