38bbfd06fd9365fdef46554e3d1b02d048d59a95
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-11 13:37:01 -0600

    Create libs.js

---
+++ mad-lib/js/libs.js
@@ -0,0 +1,9 @@
+var noun1 = prompt("Enter a noun: ");
+var noun2 = prompt("Enter a noun: ");
+var verb1 = prompt("Enter a verb: ");
+var verb2 = prompt("Enter a verb: ");
+var adj1 = prompt("Enter an adjective: ");
+var adj1 = prompt("Enter an adjective: ");
+var madlib1 = "There was a " + adj1 + " boy named Bryan. Bryan had to " + verb1 + ". Before he could do that, he needed to find his " + adj2 + " " + noun2 + "Afterwards, Bryan went for a " + verb2 + " with his " + noun1;
+
+document.getElementById('output').innerHTML = madlib1;
