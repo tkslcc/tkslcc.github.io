f84b7b86e6bd3b397bc5ba08badda174d5ac2232
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-11 14:09:07 -0600

    Update libs.js

--- mad-lib/js/libs.js
+++ mad-lib/js/libs.js
@@ -1,9 +1,21 @@
-var noun1 = prompt("Enter a noun: ");
-var noun2 = prompt("Enter a noun: ");
-var verb1 = prompt("Enter a verb: ");
-var verb2 = prompt("Enter a verb: ");
-var adj1 = prompt("Enter an adjective: ");
-var adj1 = prompt("Enter an adjective: ");
-var madlib1 = "There was a " + adj1 + " boy named Bryan. Bryan had to " + verb1 + ". Before he could do that, he needed to find his " + adj2 + " " + noun2 + "Afterwards, Bryan went for a " + verb2 + " with his " + noun1;
+var game = prompt("Type 1 to play game #1, or 2 to play game #2");
 
-document.getElementById('output').innerHTML = madlib1;
+if(game == 1){
+	var noun1 = prompt("Enter a noun: ");
+	var noun2 = prompt("Enter a noun: ");
+	var verb1 = prompt("Enter a verb: ");
+	var verb2 = prompt("Enter a verb: ");
+	var adj1 = prompt("Enter an adjective: ");
+	var adj2 = prompt("Enter an adjective: ");
+	var madlib1 = "There was a " + adj1 + " boy named Bryan. Bryan had to " + verb1 + ". Before he could do that, he needed to find his " + adj2 + " " + noun2 + ". Afterwards, Bryan went for a " + verb2 + " with his " + noun1 + ".";
+}
+else if(game == 2){
+	var noun1 = prompt("Enter a noun: ");
+	var noun2 = prompt("Enter a noun: ");
+	var verb1 = prompt("Enter a verb: ");
+	var verb2 = prompt("Enter a verb: ");
+	var adj1 = prompt("Enter an adjective: ");
+	var adj2 = prompt("Enter an adjective: ");
+	var madlib1 = "Once upon a time, a " + adj1 + " cat named Henry went to " + verb1 + ". Upon doing so, he found a " + adj2 + " " + noun2 + ". Afterwards, he decided to " + verb2 + " with his " + noun1 + ".";
+}
+	
