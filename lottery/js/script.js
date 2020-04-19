dc0a48d5549cc06ae02f10dfe76b329d10b18e67
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-19 17:00:45 -0600

    Update script.js

--- lottery/js/script.js
+++ lottery/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
@@ -25,3 +26,32 @@ for (var i = 0; i < lotNums.length; i++){
 	}
 }
 
+=======
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+var nums = prompt("How many numbers do you want to pick? (Must be less than 10)");
+
+if (nums >= 10)
+	alert("Invalid number");
+
+var lotNums = [];
+var lottoPicks = " ";
+
+for (var i = 0; i < nums; i++){
+	lotNums[i] = getRandomInt(nums);
+}
+
+
+for (var i = 0; i < lotNums.length; i++){
+	if (i == (lotNums.length - 1)){
+		lottoPicks += lotNums[i];
+	}
+	else{
+		lottoPicks = lottoPicks + lotNums[i] + "-";
+	}
+}
+alert(lottoPicks);
+>>>>>>> 53ed971bceb81cf569a174bdd2ff2821e0eda651
