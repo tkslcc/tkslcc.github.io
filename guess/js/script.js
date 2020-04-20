d42b047615c4b843eb000dca1fe27dd7d68ae002
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-20 17:13:19 -0600

    Update script.js

--- guess/js/script.js
+++ guess/js/script.js
@@ -1,59 +1,50 @@
-<<<<<<< HEAD
 function getRandomInt() {
   return Math.floor(Math.random() * Math.floor(99));
 }
-
+function restart() {
+	document.getElementById("button1").style.display = 'none';
+	num = getRandomInt();
+	document.getElementById("pic").src="img/start.jpg";
+	document.getElementById("outcome").innerHTML="";
+	document.getElementById("guess").value="";
+	console.log(num);
+	guesses = 0;
+}
+function switchImage()
+{
+	document.getElementById("guesscount").innerHTML= guessedNums;
+	guesses += 1;
+	document.getElementById("guesscount").innerHTML= "Guess counter: " + guesses;
+	if(document.getElementById("guess").value == num){
+		if (guesses == 1){
+			document.getElementById("pic").src="img/nice.jpg";
+			document.getElementById("outcome").innerHTML="Correct! You got it in one try!";
+			document.getElementById("button1").style.display = 'block';
+		}
+		else {
+			document.getElementById("pic").src="img/correct.jpg";
+			document.getElementById("outcome").innerHTML="Correct!";
+			document.getElementById("button1").style.display = 'block';
+		}
+	}
+	if(document.getElementById("guess").value > num){
+		document.getElementById("pic").src="img/toohigh.jpg";
+		document.getElementById("outcome").innerHTML="Too high!";
+		guessedNums[guessedNums.length] = document.getElementById("guess").value;
+	}
+	if(document.getElementById("guess").value < num){
+		document.getElementById("pic").src="img/toolow.jpg";
+		document.getElementById("outcome").innerHTML="Too low!";
+		guessedNums[guessedNums.length] = document.getElementById("guess").value;
+	}
+	if(document.getElementById("guess").value % 1 != 0){
+		document.getElementById("pic").src="img/error.jpg";
+		document.getElementById("outcome").innerHTML="Numbers only!";
+	}
+	document.getElementById("guesses").innerHTML= "Previous guesses: " + guessedNums;
+}
+var guessedNums = [];
 var num = getRandomInt();
-
-
-/**
-var nums = prompt("How many numbers do you want to pick? (Must be less than 10)");
-
-if (nums >= 10)
-	alert("Invalid number");
-
-var lotNums = [];
-var lottoPicks = " ";
-
-for (var i = 0; i < nums; i++){
-	lotNums[i] = getRandomInt(nums);
-}
-
-
-for (var i = 0; i < lotNums.length; i++){
-	if (i == (lotNums.length - 1)){
-		lottoPicks += lotNums[i];
-	}
-	else{
-		lottoPicks = lottoPicks + lotNums[i] + "-";
-	}
-}
-**/
-=======
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-
-var nums = prompt("How many numbers do you want to pick? (Must be less than 10)");
-
-if (nums >= 10)
-	alert("Invalid number");
-
-var lotNums = [];
-var lottoPicks = " ";
-
-for (var i = 0; i < nums; i++){
-	lotNums[i] = getRandomInt(nums);
-}
-
-
-for (var i = 0; i < lotNums.length; i++){
-	if (i == (lotNums.length - 1)){
-		lottoPicks += lotNums[i];
-	}
-	else{
-		lottoPicks = lottoPicks + lotNums[i] + "-";
-	}
-}
->>>>>>> aab3e847896e75e024709cd2479e434f63d70537
+var guesses = 0;
+console.log(num);
+alert(num);
