4ec5b9f533f4b24e0c63c1061c21436946938bcc
tkslcc <60019174+tkslcc@users.noreply.github.com>
2020-04-20 12:09:25 -0600

    Update script.js

--- guess/js/script.js
+++ guess/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function getRandomInt() {
   return Math.floor(Math.random() * Math.floor(99));
 }
@@ -28,3 +29,31 @@ for (var i = 0; i < lotNums.length; i++){
 	}
 }
 **/
+=======
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+var nums = prompt("How many numbers do you want to pick? (Must be less than 10)");
+
+if (nums >= 10)
+	alert("Invalid number");
+
+var lotNums = [];
+var lottoPicks = " ";
+
+for (var i = 0; i < nums; i++){
+	lotNums[i] = getRandomInt(nums);
+}
+
+
+for (var i = 0; i < lotNums.length; i++){
+	if (i == (lotNums.length - 1)){
+		lottoPicks += lotNums[i];
+	}
+	else{
+		lottoPicks = lottoPicks + lotNums[i] + "-";
+	}
+}
+>>>>>>> aab3e847896e75e024709cd2479e434f63d70537
